0a21bc3ddbe94ba7bdd2bf3f795035310f265a38
cristian-pgit <112448130+cristian-pgit@users.noreply.github.com>
2023-01-13 11:48:00 -0300

    Add files via upload

---
+++ Assets/js/main.js
@@ -0,0 +1,39 @@
+// Modifica alerta al enviar mensaje en index usando Sweetalert
+function envioMsj() {
+  var text = document.getElementById("exampleFormControlTextarea1").value;
+  if (text != "") {
+    swal("Team Cachureando", ";) Anotado!", "success", {button: "Vale!"});
+  } else {
+    swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
+  }
+}
+
+//Modifica alerta al enviar mensaje en contacto usando Sweetalert
+function enviCont() {
+    var text = document.getElementById("mensaje").value;
+    if (text != "") {
+      swal("Team Cachureando", ";) Formulario Enviado!", "success", {button: "Vale!"});
+    } else {
+      swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
+    }
+}
+
+//Modifica alerta al enviar mensaje en Productos usando Sweetalert
+function compraZa() {
+    var text = document.getElementById("mensaje").value;
+    if (text != "") {
+      swal("Team Cachureando", ";) Formulario Enviado!", "success", {button: "Vale!"});
+    } else {
+      swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
+    }
+}
+
+//Modifica alerta al enviar mensaje en Encuesta usando Sweetalert
+function envieEncuesta() {
+    var text = document.getElementById("encuesta").value;
+    if (text != "") {
+      swal("Team Cachureando", ";) Encuesta Enviada!", "success", {button: "Gracias!"});
+    } else {
+      swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
+    }
+}
