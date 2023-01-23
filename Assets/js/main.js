b39b94f3f8b30b48c83058cd212a718956ddfb4c
Cristian Carrillo <naitsircl@gmail.com>
2023-01-23 12:12:56 -0300

    update 23 f1

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -26,10 +26,31 @@ function compraZa() {
 
 //Modifica alerta al enviar mensaje en Encuesta usando Sweetalert
 function envioEncuesta() {
-   swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
+  var text = document.getElementById("exampleFormControlInput1").value;
+  if (text != "") {
+    swal("Team Cachureando", ";) Enviada!", "success", {button: "Vale!"});
+  } else {
+    swal("Team Cachureando", "Soci@! le falta ingresar el email", "error");
+  }
 }
 
 //Modifica alerta al enviar mensaje en equipo usando Sweetalert
 function contactame() {
   swal("Team Cachureando", "Soci@! le falta ingresar el mensaje", "error");
 }
+//muestra un reloj en la barra de navegacion
+function startTime(){
+  today=new Date();
+  h=today.getHours();
+  m=today.getMinutes();
+  s=today.getSeconds();
+  m=checkTime(m);
+  s=checkTime(s);
+  document.getElementById('reloj1').innerHTML=h+":"+m+":"+s;
+  document.getElementById('reloj2').innerHTML=h+":"+m+":"+s;
+  t=setTimeout('startTime()',500);}
+  function checkTime(i)
+  {if (i<10) {i="0" + i;}return i;}
+  window.onload=function(){startTime();}
+
+  //
